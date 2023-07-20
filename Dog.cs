0a11f141931d5541a9202dd021afef347d4dd2a4
skafula <79100340+skafula@users.noreply.github.com>
2023-07-20 11:41:09 +0200

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,36 @@
+﻿public class Dog
+{
+    string _name;
+    string _owner;
+    int _age;
+
+    public string Name { get; set; }
+    public string Owner { get; set; }  
+    
+    public int Age { get; set; }
+
+    public Dog()
+    {
+
+    }
+
+    public Dog(string name)
+    {
+        Name = name;
+    }
+    public bool FirstDogIsOlder(Dog dog, Dog otherDog)
+    {
+        return dog > otherDog;
+    }
+    public static bool operator >(Dog dog, Dog otherDog) 
+        => dog?.Age > otherDog?.Age;
+
+    public static bool operator <(Dog dog, Dog otherDog)
+        => dog?.Age < otherDog?.Age;
+
+    public static bool operator ==(Dog dog, Dog otherDog)
+        => dog?.Age == otherDog?.Age;
+
+    public static bool operator !=(Dog dog, Dog otherDog)
+        => !(dog == otherDog);
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPractice
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Dog dog1 = new() { Age = 5 };
+            Dog dog2 = new() { Age = 7 };
+
+            Console.WriteLine(dog1 > dog2);
+            Console.WriteLine(dog1 < dog2);
+            Console.WriteLine(dog1 == dog2);
+            Console.WriteLine(dog1 != dog2);
+
+            Console.WriteLine($"First dog is older? - {dog1 > dog2}");
+            Console.WriteLine($"First dog is younger? - {dog1 < dog2}");
+
+        }
+    }
+}
